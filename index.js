f0c945970533b291475300cbd9085b4f88b3bb85
Steve Alomia <stevealomia@yahoo.com>
2022-03-15 11:00:45 -0400

    commit tree 8cc80ab4c49173686f8e77977d212086afe37664

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(number){
+  let result = number ;
+  if (result <= 400) {
+    return 'This one is on me!';
+  } else if (result >= 2000 && result <= 2500 ){
+    return 'I will gladly take your thirty bucks.';
+  } else {
+  return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city){
+   return (city === 'NYC' ? 'Ok, sounds good.': 'No go.')
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.';
+      
+  }
 }
